34b98997846d75c4013f9f10147677ffe0104d2d
hummingbird1986 <39380985+hummingbird1986@users.noreply.github.com>
2018-05-28 12:02:59 +0800

    Added F1-F12,Eject,Arrow and Caps to the mapping.

--- QMK/keymap.c
+++ QMK/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS, \
   CTL_T(KC_ESC), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,  KC_ENT,  \
   KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,\
-  TT(2), KC_LALT,KC_LGUI,                KC_SPC,                                 KC_RGUI,KC_RALT,KC_RCTL,KC_DEL),
+  TT(2), KC_LALT,KC_LGUI,                KC_SPC,                                 KC_RGUI,KC_RALT,KC_CAPS,KC_DEL),
 
   /* Keymap _FL: Function Layer
    * ,-----------------------------------------------------------.
@@ -70,11 +70,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,_______,_______,                _______,                        _______,_______,_______,_______),
 
   [_NL] = KEYMAP_ANSI(
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,  \
+  _______,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,KC_EJCT,  \
   _______,_______,_______,_______,_______,_______,_______,   KC_7,   KC_8,   KC_9,_______,_______,_______,_______, \
   _______,_______,_______,_______,_______,_______,_______,   KC_4,   KC_5,   KC_6,_______,_______,_______, \
-  _______,_______,_______,_______,_______,_______,_______,   KC_1,   KC_2,   KC_3,_______,_______, \
-  _______,_______,_______,                   KC_0,                         KC_DOT,_______,_______,_______),
+  _______,_______,_______,_______,_______,_______,_______,   KC_1,   KC_2,   KC_3,_______,  KC_UP, \
+  _______,_______,_______,                   KC_0,                         KC_DOT,KC_LEFT,KC_DOWN,KC_RIGHT),
 
 
 
