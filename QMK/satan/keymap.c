4a210f2bbd8bdf462a54e85d01af3ed962e2f168
Hummingbird1986.local <danieldeng@me.com>
2018-06-15 21:15:16 +0800

    update after physical added

--- QMK/satan/keymap.c
+++ QMK/satan/keymap.c
@@ -1,114 +1,45 @@
 #include "satan.h"
 
+#define KC_____ KC_TRNS
 
-// Used for SHIFT_ESC
-#define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
-
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
 #define _BL 0
-#define _ML 1 //Media Layer
-#define _NL 2 //Number Layer
+#define _VIML 1
+#define _NL 2
+#define _HHKB 3
 
-#define _______ KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Keymap _BL: (Base Layer) Default Layer
-   * ,-----------------------------------------------------------.
-   * | `~ | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |
-   * |-----------------------------------------------------------|
-   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |
-   * |-----------------------------------------------------------|
-   * |ESC/Ctl|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
-   * |-----------------------------------------------------------|
-   * |SFT/CAPS|  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |
-   * |-----------------------------------------------------------|
-   * |Ctrl|ALT`|GUI |      Space            |Alt |Gui |CAPS|DEL  |
-   * `-----------------------------------------------------------'
-   */
-[_BL] = KEYMAP_ANSI(
-  KC_GRV,    KC_1,   KC_2,LT(1,KC_3),   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC, \
-  KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS, \
-  CTL_T(KC_ESC), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,  KC_ENT,  \
-  SFT_T(KC_CAPS),         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,\
-  TT(2), KC_LALT,KC_LGUI,                KC_SPC,                                 KC_RGUI,KC_RALT,KC_CAPS,KC_DEL),
+    // 0: qwerty
+    [_BL]=KEYMAP(
+        KC_GRV,         KC_1,   KC_2,LT(1,KC_3),   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL ,KC_BSPC,  KC_NO,
+        KC_TAB,         KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,
+        CTL_T(KC_ESC),  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_NO,  KC_ENT ,
+        KC_LSFT,KC_NO , KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,TT(3)  ,
+        TT(2),KC_LALT,KC_LGUI,                        KC_SPC,                         KC_RGUI,KC_RALT,KC_CAPS,KC_DEL
+    ),
+    // 1: fn layer
+    [_VIML]=KEYMAP(
+        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+        _______,_______,_______,_______,_______,_______,KC_LEFT,KC_DOWN,  KC_UP,KC_RIGHT,_______,_______,_______,_______,
+        _______,_______,_______,_______,_______,_______,KC_HOME,KC_PGDN,KC_PGUP,_______ ,_______,_______,_______,_______,
+        _______,_______,_______,                        _______,                        _______,_______,_______,_______
+    ),
+    
+    
+    [_NL]=KEYMAP(
+            _______,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,_______,_______,
+            _______,KC_MPLY,KC_MRWD,KC_MFFD,_______,_______,_______,KC_7,   KC_8,   KC_9,_______,_______,_______,KC_EJCT,
+            _______,_______,_______,_______,_______,_______,_______,KC_4,   KC_5,   KC_6,_______,_______,_______,_______,
+            _______,_______,KC_MUTE,KC_VOLD,KC_VOLU,_______,_______,_______,KC_1,   KC_2,   KC_3,_______,KC_UP,_______,
+            _______,_______,_______,                        KC_0,                        KC_DOT,KC_LEFT,KC_DOWN,KC_RIGHT
+            ),
 
-  /* Keymap _FL: Function Layer
-   * ,-----------------------------------------------------------.
-   * |   |   |   |   |   |   |   |   |   |   |   |   |   |  RESET|
-   * |-----------------------------------------------------------|
-   * |     |   |   |   |   |   |   |   |   |   |   |BL-|BL+|BL   |
-   * |-----------------------------------------------------------| * |      |   |   |   |   |   |   |       |   |   |   |        | * |-----------------------------------------------------------| * |        | F1|F2 | F3|F4 | F5| F6| F7| F8|   |   |          |
-   * |-----------------------------------------------------------|
-   * |    |    |    |                        |    |    |    |    |
-   * `-----------------------------------------------------------'
-   */
-/*[_FL] = KEYMAP_ANSI(
-  #ifdef RGBLIGHT_ENABLE
-  KC_GRV, _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,RESET,  \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, BL_DEC,BL_INC, BL_TOGG, \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______, \
-  _______,RGB_TOG,RGB_MOD,RGB_HUI,RGB_HUD,RGB_SAI,RGB_SAD,RGB_VAI,RGB_VAD,_______,_______,_______, \
-  _______,_______,_______,                 _______,                       _______,_______,_______, _______),
-  #else
-  KC_GRV, _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,RESET,  \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, BL_DEC, BL_INC,BL_TOGG, \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
-  _______,_______,_______,                _______,                        _______,_______,_______,_______),
-  #endif*/
-  [_ML] = KEYMAP_ANSI(
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,  \
-  _______,_______,_______,_______,_______,_______,KC_MUTE,KC_VOLD,KC_VOLU,_______,_______,_______,_______,_______, \
-  _______,_______,_______,_______,_______,_______,KC_LEFT,KC_DOWN, KC_UP,KC_RIGHT,_______,_______,_______, \
-  _______,_______,_______,_______,_______,_______,KC_HOME,KC_PGDN,KC_PGUP,_______,_______,_______, \
-  _______,_______,_______,                _______,                        _______,_______,_______,_______),
-
-  [_NL] = KEYMAP_ANSI(
-  _______,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,_______,  \
-  _______,_______,_______,_______,_______,_______,_______,   KC_7,   KC_8,   KC_9,_______,_______,_______,KC_EJCT, \
-  _______,_______,_______,_______,_______,_______,_______,   KC_4,   KC_5,   KC_6,_______,_______,_______, \
-  _______,_______,_______,_______,_______,_______,_______,   KC_1,   KC_2,   KC_3,_______,  KC_UP, \
-  _______,_______,_______,                   KC_0,                         KC_DOT,KC_LEFT,KC_DOWN,KC_RIGHT),
-
-
-
+    [_HHKB]=KEYMAP(
+            KC_POWER,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,_______,_______,
+            KC_CAPS,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,  KC_UP,_______,KC_EJCT,
+            _______,_______,_______,_______,_______,_______,_______,_______,KC_HOME,KC_PGUP,KC_LEFT,KC_RIGHT,_______,_______,
+            _______,_______,_______,_______,_______,_______,_______ ,_______,_______,KC_END,KC_PGDN,KC_DOWN,_______,_______,
+            _______,_______,_______,                        _______,                        _______,_______,_______,_______
+            ),
 };
-
-/*
- *This command is for the Shift-ESC(~)
-enum function_id {
-    SHIFT_ESC,
-};
-
-const uint16_t PROGMEM fn_actions[] = {
-  [0]  = ACTION_FUNCTION(SHIFT_ESC),
-};
-
-void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
-  static uint8_t shift_esc_shift_mask;
-  switch (id) {
-    case SHIFT_ESC:
-      shift_esc_shift_mask = get_mods()&MODS_CTRL_MASK;
-      if (record->event.pressed) {
-        if (shift_esc_shift_mask) {
-          add_key(KC_GRV);
-          send_keyboard_report();
-        } else {
-          add_key(KC_ESC);
-          send_keyboard_report();
-        }
-      } else {
-        if (shift_esc_shift_mask) {
-          del_key(KC_GRV);
-          send_keyboard_report();
-        } else {
-          del_key(KC_ESC);
-          send_keyboard_report();
-        }
-      }
-      break;
-  }
-}*/
